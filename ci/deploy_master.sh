bd1363b6c5c56dc525578c3fc17109aca4bb0c89
since <1336594801@qq.com>
2022-10-27 08:22:18 +0800

    deploy

---
+++ ci/deploy_master.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#mv ./backend.jar ./ci/backend.jar
+#cd ci
+#docker build -t $IMAGE_REPO_PREFIX-master:$IMAGE_VER .
+#docker stop $CONTAINER_NAME_PREFIX-master-1 || true && docker rm $CONTAINER_NAME_PREFIX-master-1 || true
+#docker run -d -p 18889:8080 --name $CONTAINER_NAME_PREFIX-master-1 --restart unless-stopped $IMAGE_REPO_PREFIX-master:$IMAGE_VER
+#cd ..
+echo "Not ready for production ..."
+false
